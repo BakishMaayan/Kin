2a08bf2ae1a1a7548847cf410dbe9b2122443c27
BakishMaayan <44134110+BakishMaayan@users.noreply.github.com>
2018-12-01 18:22:15 +0200

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-sudo sleep 30 ; sudo curl 127.0.0.1:80
-sudo sleep 30 ; sudo curl $(dig +short myip.opendns.com @resolver1.opendns.com)
+sudo sleep 120 ; sudo curl 127.0.0.1:80
+sudo sleep 120 ; sudo curl $(dig +short myip.opendns.com @resolver1.opendns.com)
